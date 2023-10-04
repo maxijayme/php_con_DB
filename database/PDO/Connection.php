3f70b5f69978a2650a1d3be4af5d6832893fe315
maxijayme <maxijayme@gmail.com>
2023-10-04 20:42:07 +0200

    change db connection to clase. add enums

--- database/PDO/Connection.php
+++ database/PDO/Connection.php
@@ -1,23 +1,57 @@
 <?php
 
+namespace Databse\PDO;
+
 require __DIR__ . '/../../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
 $dotenv->load();
 
-$server=$_ENV['DB_HOST'];
-$username=$_ENV['DB_USERNAME'];
-$password=$_ENV['DB_PASSWORD'];
-$database=$_ENV['DB_NAME'];
-try{
-    $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
-    $setnames = $conn->prepare("SET NAMES utf8");
-    $setnames->execute();
-    var_dump($setnames);
-}
-catch(PDOException $e){
-    die("Error: " . $e->getMessage());
-} 
-finally{
-    $conn = NULL;
+
+class Connection{
+    private static $instance;
+    private $connection;
+    private function __construct(){
+        $this->make_connection();
+        $this->set_character($this->connection);
+        $this->check_connection($this->connection);
+    }
+
+    public static function getInstance(){
+        if(!self::$instance instanceof self){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+   
+    private function make_connection(){
+        $server=$_ENV['DB_HOST'];
+        $username=$_ENV['DB_USERNAME'];
+        $password=$_ENV['DB_PASSWORD'];
+        $database=$_ENV['DB_NAME'];
+        try{
+            $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+            $this->connection = $conn;
+        }
+        catch(PDOException $e){
+            die("Error: " . $e->getMessage());
+        } 
+        finally{
+            $conn = NULL;
+        }
+    }
+
+    private function set_character($connection){
+        $setnames = $connection->prepare("SET NAMES utf8");
+        $setnames->execute();
+    }
+
+    private function check_connection($connection){
+        if($connection->connect_errno)
+            die("Hubo un problema: {$connection->connect_error}");
+    }
+    
+    public function getConnection(){
+        return $this->connection;
+    }
 }
