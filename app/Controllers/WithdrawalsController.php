7e9223c969df1046643f681d6f320551ecdf8ee1
maxijayme <maxijayme@gmail.com>
2023-10-05 17:55:32 +0200

    withdrawal store with PDO avoiding injection and binding

--- app/Controllers/WithdrawalsController.php
+++ app/Controllers/WithdrawalsController.php
@@ -17,7 +17,12 @@ class WithdrawalsController{
             :date,
             :amount,
             :description)");
-        $stmt->execute($data);
+        $stmt->bindParam(":payment_method", $data['payment_method']);
+        $stmt->bindParam(":type", $data['type']);
+        $stmt->bindParam(":date", $data['date']);
+        $stmt->bindParam(":amount", $data['amount']);
+        $stmt->bindParam(":description", $data['description']);
+        $stmt->execute();
     }
     public function edit(){}
 }
